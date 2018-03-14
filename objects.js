dd5fed8da4125a4a1f6ee59e0e85724b0f0eb153
tarheelnc83@outlook.com <tarheelnc83@outlook.com>
2018-03-14 18:25:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,9 @@
 var playlist={Ganga:"Carry you home"};
 
 function updatePlaylist (object, artistName, songTitle ){
+  object.artistName=songTitle;
 
+  function updatePlaylist (object, artistName, songTitle ){
+  return
 
 }
